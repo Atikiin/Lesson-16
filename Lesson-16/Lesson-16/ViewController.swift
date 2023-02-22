b8c19cea9a58cf6e0256602988de73fba4283c3b
Atikiin <124789701+Atikiin@users.noreply.github.com>
2023-02-22 19:15:17 +0300

    Add files via upload

---
+++ Lesson-16/Lesson-16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson-16
+//
+//  Created by Никита Корольков on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
